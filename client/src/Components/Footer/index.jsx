9e377068e14d4b4e5f0a461548319c3a56210d0d
José Isaac <joseisaacnascimento@gmail.com>
2025-07-22 17:18:23 -0300

    feat: Adiciona favicons, imagens estáticas na pasta assets

---
+++ client/src/Components/Footer/index.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        © 2025
+        <a target="_blank" href="https://github.com/joseisaacpy">
+          joseisaacpy
+        </a>{" "}
+        – Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/Components/Header/index.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        {/* Logo */}
+        {/* Navigation */}
+        <nav>
+          {/* Ul */}
+          <ul>
+            <li>
+              <a className="transition" 
+              href=""></a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
