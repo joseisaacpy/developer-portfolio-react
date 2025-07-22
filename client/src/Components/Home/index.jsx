1f9df55bd17aa301e2fbfc58ac4f230cfde222b5
José Isaac <joseisaacnascimento@gmail.com>
2025-07-22 20:24:48 -0300

    feat: Adiciona animações e conteúdo à seção Sobre, incluindo Lottie e ícones

--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -1,23 +1,22 @@
 // Components
-
 const Home = () => {
   return (
     <>
       <section className="relative flex flex-col items-center justify-center h-[100vh] bg-cover bg-center bg-no-repeat bg-[url('https://joseisaacpy.github.io/developer-portfolio/img/hero/hero-image.webp')] text-white">
-        <div class="absolute inset-0 bg-black/50"></div>
-        <div class="p-2 relative z-10 flex flex-col gap-2 items-center justify-center h-full text-white text-center">
-          <h1 class="text-4xl md:text-6xl font-extrabold text-center">
-            Olá, me chamo <span class="text-blue-500">José Isaac</span>!
-            <span class="wave-hand">👋🏽</span>
+        <div className="absolute inset-0 bg-black/50"></div>
+        <div className="p-2 relative z-10 flex flex-col gap-2 items-center justify-center h-full text-white text-center">
+          <h1 className="text-4xl md:text-6xl font-extrabold text-center">
+            Olá, me chamo <span className="text-blue-500">José Isaac</span>!
+            <span className="wave-hand">👋🏽</span>
           </h1>
-          <p class="text-xl md:text-2xl font-medium">
+          <p className="text-xl md:text-2xl font-medium">
             Desenvolvedor{" "}
-            <span class="text-blue-500 font-bold">Full Stack</span>
+            <span className="text-blue-500 font-bold">Full Stack</span>
           </p>
-          <p class="text-lg md:text-xl text-gray-200">
+          <p className="text-lg md:text-xl text-gray-200">
             Transformando ideias em códigos e tomando um cafezinho.
           </p>
-          <p class="text-lg md:text-xl text-blue-500 font-bold">
+          <p className="text-lg md:text-xl text-blue-500 font-bold">
             Vamos criar algo juntos?
           </p>
         </div>
